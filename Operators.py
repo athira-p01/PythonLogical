5c67cfae94d0a0e8dd82c172cebe16241d0dea37
athira-p01 <you@example.com>
2024-07-05 11:04:33 +0530

    first commit

---
+++ Operators.py
@@ -0,0 +1,59 @@
+# x=int(input("enter x :"))
+# y=int(input("enter y :"))
+# sum=x+y
+# product=x*y
+# division=x/y
+# rem=x%y
+# exp=x**y
+# floor=x//y
+# print("sum of x and y =",sum)
+# print("product of x and y =",product)
+# print("division of x and y =",division)
+# print("reminder =",rem)
+# print("exponent =",exp)
+# print("floor =",floor)
+
+
+# x=4
+# y=5
+# x=int(input("enter x :"))
+# y=int(input("enter y :"))
+# print("equal to :",x==y)
+# print("not equal to :",x!=y)
+# print("less than or equal :",x<=y)
+# print("greater than or equal :",x>=y)
+# print("less than :",x<y)
+# print("greater than :",x>y)
+
+# x=5
+# y=2
+# a=4
+# b=5
+# c=3
+# d=4
+# e=2
+# x+=5
+# y-=2
+# a*=8
+# b/=7
+# c%=5
+# d**=3
+# e//=7
+# print(x)
+# print(y)
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+
+# a=7
+# b=6
+# print(a&b)
+# print(a|b)
+# print(a^b)
+
+pi=3.14
+r=int(input("radius of the circle :"))
+area=pi*r*r
+print("area of the circle :",area)
