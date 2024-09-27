3312e3ffc8b8cf608a2d4c0d25a1c5b6e4c38291
athira-p01 <athiraprabha001@gmail.com>
2024-09-27 23:51:16 +0530

    Add files via upload

---
+++ Reverse right angle num pattern.py
@@ -0,0 +1,33 @@
+# n = int(input("Enter the rows : "))
+# for i in range(n):
+#     for j in range(n-i):
+#         print(j+1, end=" ")
+#     print()
+
+
+
+
+# n = 5
+# for i in range(n):
+#     for j in range(n-i):
+#         print(i+1, end=" ")
+#     print()
+
+
+
+
+n = 5
+for i in range(n):
+    for j in range(n-i):
+        print(n-i, end=" ")
+    print()
+
+
+
+
+
+n = 5
+for i in range(n):
+    for j in range(n-i):
+        print(n-j, end=" ")
+    print()
---
+++ Right angle num pattern.py
@@ -0,0 +1,32 @@
+# n = 5
+# for i in range(n):
+#     for j in range(i+1):
+#         print(n-j, end=" ")
+#     print()
+
+
+
+
+# n = 5
+# for i in range(n):
+#     for j in range(i, -1, -1):
+#         print(n-j, end=" ")
+#     print()
+
+
+
+
+n = 5
+for i in range(n):
+    for j in range(i+1):
+        print(j+1, end=" ")
+    print()
+
+
+
+
+n = 5
+for i in range(n):
+    for j in range(i, -1, -1):
+        print(j+1, end=" ")
+    print()
